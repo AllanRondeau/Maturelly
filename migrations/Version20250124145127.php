4f52fff74d0a4a1a91314902eba2dc24a93677c2
allan <arondeau@wemanity.com>
2025-01-24 16:05:37 +0100

    Chores: install security bundle Chores: install easy admin Feat: import login and register form

---
+++ migrations/Version20250124145127.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250124145127 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER INDEX uniq_identifier_uuid RENAME TO UNIQ_IDENTIFIER_EMAIl');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER INDEX uniq_identifier_email RENAME TO uniq_identifier_uuid');
+    }
+}
