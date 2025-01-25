a5ebb5d9fcf54702dc04a1b84965f3690e94ff11
allan <arondeau@wemanity.com>
2025-01-25 16:15:25 +0100

    Feat: landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
 module.exports = {
   content: [
     "./assets/**/*.js",
     "./templates/**/*.html.twig",
   ],
   theme: {
+    colors: {
+      primary: '#FE0039',
+      secondary: '#011936',
+      ...colors
+    },
+    fontFamily: {
+      'nunito': ['Nunito', 'sans-serif'],
+      'anticSlab': ['Antic Slab', 'serif']
+    },
     extend: {},
   },
   plugins: [],
