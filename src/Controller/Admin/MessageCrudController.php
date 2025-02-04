b26ee76377f01c8ec551b7ed6a736a303a2a1696
allan <arondeau@wemanity.com>
2025-02-04 12:33:02 +0100

    Feat: Import user and messages in admin

---
+++ src/Controller/Admin/MessageCrudController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Message;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class MessageCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Message::class;
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextareaField::new('content'),
+            AssociationField::new('sender')
+            ->setFormTypeOption('disabled', true)
+            ->setLabel('name')
+        ];
+    }
+
+}
